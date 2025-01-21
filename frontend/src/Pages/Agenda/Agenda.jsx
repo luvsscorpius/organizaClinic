5157986c01a9a861512d7aa56f6136f22205d8cc
and <andersonvittor1801@gmail.com>
2025-01-21 19:10:12 -0300

    feat(agenda.jsx): implementing modal

--- frontend/src/Pages/Agenda/Agenda.jsx
+++ frontend/src/Pages/Agenda/Agenda.jsx
@@ -3,23 +3,19 @@ import * as H from '../Home/Styles';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-
-import {
-    Button,
-    Dialog,
-    DialogHeader,
-    DialogBody,
-    DialogFooter,
-} from "@material-tailwind/react";
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import '../../Css/index.css'
 
 export const Agenda = () => {
     const [events, setEvents] = useState([]);
     const [open, setOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null)
 
     const handleOpen = () => setOpen(!open);
 
     const handleDateClick = (arg) => {
-        alert(arg.dateStr);
+        setSelectedDate(arg.dateStr)
+        handleOpen()
     };
 
     return (
@@ -31,30 +27,125 @@ export const Agenda = () => {
                 selectable={true}
             />
 
-            <Button onClick={handleOpen} variant="gradient">
-                Open Modal
-            </Button>
-            <Dialog open={open} handler={handleOpen}>
-                <DialogHeader>Its a simple modal.</DialogHeader>
-                <DialogBody>
-                    The key to more success is to have a lot of pillows. Put it this way,
-                    it took me twenty five years to get these plants, twenty five years of
-                    blood sweat and tears, and I&apos;m never giving up, I&apos;m just
-                    getting started. I&apos;m up to something. Fan luv.
-                </DialogBody>
-                <DialogFooter>
-                    <Button
-                        variant="text"
-                        color="red"
-                        onClick={handleOpen}
-                        className="mr-1"
-                    >
-                        <span>Cancel</span>
-                    </Button>
-                    <Button variant="gradient" color="green" onClick={handleOpen}>
-                        <span>Confirm</span>
-                    </Button>
-                </DialogFooter>
+            <Dialog open={open} onClose={setOpen} className="relative z-10">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in "
+                />
+
+                <div className="fixed inset-0 z-10 max-w-100 overflow-y-auto place-items-center">
+                    <div className="flex min-h-full !min-w-full items-center justify-center p-4 text-center sm:items-center sm:p-0"  >
+                        <DialogPanel
+                            transition
+                            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95" 
+                        >
+                            <form className="space-y-6">
+                                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                    <div className="text-center sm:text-left">
+                                        <DialogTitle as="h3" className="text-lg font-semibold text-gray-900">
+                                            Adicionar Evento
+                                        </DialogTitle>
+                                        <div className="mt-6 space-y-4">
+                                            {/* Cliente */}
+                                            <div>
+                                                <label htmlFor="cliente" className="block text-sm font-medium text-gray-700 text-start">
+                                                    Cliente
+                                                </label>
+                                                <select
+                                                    id="cliente"
+                                                    name="cliente"
+                                                    className="mt-1 w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-sm"
+                                                >
+                                                    <option value="">Selecione um cliente</option>
+                                                    <option value="Anderson">Anderson</option>
+                                                </select>
+                                            </div>
+
+                                            {/* Médico */}
+                                            <div>
+                                                <label htmlFor="medico" className="block text-sm font-medium text-gray-700 text-start">
+                                                    Médico
+                                                </label>
+                                                <select
+                                                    id="medico"
+                                                    name="medico"
+                                                    className="mt-1 w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-sm"
+                                                >
+                                                    <option value="">Selecione um médico</option>
+                                                    <option value="Anderson">Anderson</option>
+                                                </select>
+                                            </div>
+
+                                            {/* Data */}
+                                            <div>
+                                                <label htmlFor="data" className="block text-sm font-medium text-gray-700 text-start">
+                                                    Data
+                                                </label>
+                                                <input
+                                                    type="date"
+                                                    id="data"
+                                                    name="data"
+                                                    value={selectedDate}
+                                                    readOnly
+                                                    className="mt-1 w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-sm"
+                                                />
+                                            </div>
+
+                                            {/* Horário */}
+                                            <div>
+                                                <label htmlFor="horario" className="block text-sm font-medium text-gray-700 text-start">
+                                                    Horário
+                                                </label>
+                                                <select
+                                                    id="horario"
+                                                    name="horario"
+                                                    className="mt-1 w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-sm"
+                                                >
+                                                    <option value="">Selecione um horário</option>
+                                                    {[7, 8, 9, 10, 11, 13, 14, 15, 16, 17].map((hour) => (
+                                                        <option key={hour} value={hour}>
+                                                            {`${hour}:00`}
+                                                        </option>
+                                                    ))}
+                                                </select>
+                                            </div>
+
+                                            {/* Descrição */}
+                                            <div>
+                                                <label htmlFor="description" className="block text-sm font-medium text-gray-700 text-start">
+                                                    Descrição do evento
+                                                </label>
+                                                <textarea
+                                                    id="description"
+                                                    name="description"
+                                                    placeholder="Descreva o evento"
+                                                    className="mt-1 w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 text-sm"
+                                                    rows="4"
+                                                ></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                    <button
+                                        type="submit"
+                                        className="inline-flex w-full justify-center rounded-md bg-[#15babc] px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                                    >
+                                        Salvar
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() => setOpen(false)}
+                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                    >
+                                        Cancelar
+                                    </button>
+                                </div>
+                            </form>
+                        </DialogPanel>
+                    </div>
+                </div>
             </Dialog>
         </H.section>
     );
