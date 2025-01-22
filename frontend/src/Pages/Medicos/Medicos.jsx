de183e96e67a593ded9b005f238275567571cce9
and <andersonvittor1801@gmail.com>
2025-01-22 18:23:07 -0300

    feat(medicos.jsx): implementing searchInput and button

--- frontend/src/Pages/Medicos/Medicos.jsx
+++ frontend/src/Pages/Medicos/Medicos.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import * as H from '../Home/Styles'
+import * as M from './Styles'
+import { FaPlus } from "react-icons/fa";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 
 export const Medicos = () => {
   return (
     <H.section>
-        Médicos
+        <M.buttonContainer>
+          <span className='searchSpan'>
+            <HiOutlineMagnifyingGlass/>
+            <input type="search" placeholder='Pesquise um médico'  />
+          </span>
+
+          <span className='buttonSpan'>
+            <FaPlus/>
+            <button>Novo</button>
+          </span>
+        </M.buttonContainer>
     </H.section>
   )
 }
--- frontend/src/Pages/Medicos/Styles.jsx
+++ frontend/src/Pages/Medicos/Styles.jsx
@@ -1,1 +1,60 @@
 import styled from "styled-components";
+
+export const buttonContainer = styled.div`
+    margin-top: 10px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    gap: 10px;
+
+    .buttonSpan {
+        width: 130px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+        padding: 5px;
+        background-color: #15babc;
+        border-radius: 5px;
+        color: #fff;
+        margin-right: 10px;
+        height: 35px;
+    }
+
+    .searchSpan {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        position: relative;
+        height: 35px;
+        margin-left: 10px;
+
+        input[type=search] {
+            border: 1px solid #ccc;
+            width: 100%;
+            padding-left: 23px;
+            border-radius: 5px;
+            height: 100%;
+
+            &:focus {
+                outline: none;
+            }
+        }
+
+        svg {
+            position: absolute;
+            left: 5px;
+            color: #aaa;
+        }
+    }
+
+    span:hover {
+        cursor: pointer;
+    }
+
+    .buttonSpan:active {
+        transform: scale(0.95)
+    }
+`
