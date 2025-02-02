1f75c0d02afbd4f2ec7d4fb8cfefd1221275cfdc
and <andersonvittor1801@gmail.com>
2025-02-02 16:23:46 -0300

    feat(backend addnewpatient route): implementing query sql on route

--- backend/Src/Routes/addNewPatient.route.js
+++ backend/Src/Routes/addNewPatient.route.js
@@ -7,12 +7,24 @@ router.post('/', async (req, res) => {
 
     try {
         const newPatient = req.body
-
-        const conn = await db()
+        const {Nome, DataDeNascimento, Genero, Sexo, CPF, Telefone, Email, Naturalidade, CEP, Rua, Numero, Bairro, Cidade, Estado, DataDeCadastro} = newPatient
 
         console.log(newPatient)
         console.log('addNewPatient route')
-        res.status(200).send('Teste')
+
+        const conn = await db()
+
+        const sql = `INSERT INTO pacientes (Nome, DataDeNascimento, Genero, Sexo, CPF, Telefone, Email, Naturalidade, CEP, Rua, Numero, Bairro, Cidade, Estado, DataDeCadastro) VALUES ('${Nome}', '${DataDeNascimento}', '${Genero}', '${Sexo}', '${CPF}', '${Telefone}',' ${Email}', '${Naturalidade}', '${CEP}', '${Rua}', '${Numero}', '${Bairro}', '${Cidade}', '${Estado}', '${DataDeCadastro}')`
+
+        await conn.query(sql, (err, result) => {
+            if (err) {
+                console.error('Erro ao adicionar dados ao banco de dados', err)
+                return
+            }
+
+            console.log('Dados inseridos ao banco de dados')
+            res.status(200).send('Dados inseridos ao banco de dados')
+        })
     } catch (error) {
         console.error(error)
     }
