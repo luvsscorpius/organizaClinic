7d8d17a3ac339461a70da5197b3306b442212626
and <andersonvittor1801@gmail.com>
2025-02-09 15:28:30 -0300

    feat(backend updatepatient.route): receiving data on the backend

--- backend/Src/Routes/updatePatient.route.js
+++ backend/Src/Routes/updatePatient.route.js
@@ -4,6 +4,15 @@ const db = require('../config/db')
 
 router.put('/:id', async (req, res) => {
     console.log('Update Patient route accessed.')
+    const id = req.params
+    const patientUpdate = req.body
+    console.log(id, patientUpdate)
+
+    try {
+        const conn = await db()
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 module.exports = router
