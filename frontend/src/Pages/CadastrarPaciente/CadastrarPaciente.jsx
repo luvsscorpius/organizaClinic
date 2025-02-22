3615f1792bd76af03b3fcab3f69aeb6311c17f0b
and <andersonvittor1801@gmail.com>
2025-02-22 15:24:57 -0300

    feat(frontend cadastrarpaciente.jsx): implementing API viaCEP do get the address

--- frontend/src/Pages/CadastrarPaciente/CadastrarPaciente.jsx
+++ frontend/src/Pages/CadastrarPaciente/CadastrarPaciente.jsx
@@ -28,7 +28,21 @@ export const CadastrarPaciente = () => {
         DataDeCadastro: date,
     })
 
-    const {pacientes, setPacientes} = useContext(OrganizaClinicContext)
+    // Feat: Implementando busca de endereço através do CEP
+    const buscarCEP = async () => {
+        if (newPatient.CEP.length === 8) {
+            try {
+                const response = await axios.get(`https://viacep.com.br/ws/${newPatient.CEP}/json/`)
+                setNewPatient((prev) => ({ ...prev, Rua: response.data.logradouro, Bairro: response.data.bairro, Cidade: response.data.localidade, Estado: response.data.estado }))
+            } catch (error) {
+                console.error("Erro ao buscar o CEP", error)
+            }
+        }
+    }
+
+    console.log(newPatient)
+
+    const { pacientes, setPacientes } = useContext(OrganizaClinicContext)
 
     const checkCPFs = (CPF) => {
         return pacientes.some((paciente) => paciente.CPF === CPF)
@@ -36,10 +50,10 @@ export const CadastrarPaciente = () => {
 
     const cadastrarPaciente = async (e) => {
         e.preventDefault()
-        
-        const {Nome, DataDeNascimento, Genero, Sexo, CPF, Telefone, Email, Naturalidade, CEP, Rua, Numero, Bairro, Cidade, Estado, DataDeCadastro} = newPatient
 
-        if (Nome === '' || DataDeNascimento === '' || Genero === '' || Sexo === '' || CPF === '' || Telefone === '' || Email === ''|| Naturalidade === '' || CEP === '' || Rua === '' || Numero === '' || Bairro === '' || Cidade === '' || Estado === '', DataDeCadastro === '') {
+        const { Nome, DataDeNascimento, Genero, Sexo, CPF, Telefone, Email, Naturalidade, CEP, Rua, Numero, Bairro, Cidade, Estado, DataDeCadastro } = newPatient
+
+        if (Nome === '' || DataDeNascimento === '' || Genero === '' || Sexo === '' || CPF === '' || Telefone === '' || Email === '' || Naturalidade === '' || CEP === '' || Rua === '' || Numero === '' || Bairro === '' || Cidade === '' || Estado === '', DataDeCadastro === '') {
             toast.error('Preencha todos os dados antes de salvar.')
         } else if (CPF.length < 11) {
             toast.error('Campo CPF precisa ter 11 digitos.')
@@ -47,9 +61,9 @@ export const CadastrarPaciente = () => {
             toast.error('CPF já cadastrado no banco de dados.')
         } else {
             try {
-                const res = await axios.post('http://localhost:2000/addNewPatient',newPatient, {
-                    headers: {'Content-Type': 'application/json'}
-                }) 
+                const res = await axios.post('http://localhost:2000/addNewPatient', newPatient, {
+                    headers: { 'Content-Type': 'application/json' }
+                })
 
                 if (res.status === 200) {
                     navigate('/Pacientes')
@@ -66,7 +80,7 @@ export const CadastrarPaciente = () => {
     const navigate = useNavigate()
 
     return (
-        <H.section style={{flexDirection: 'column'}}>
+        <H.section style={{ flexDirection: 'column' }}>
             <C.ButtonContainer>
                 <span onClick={() => navigate('/Pacientes')}>
                     <MdOutlineArrowBack size={22} />
@@ -83,16 +97,16 @@ export const CadastrarPaciente = () => {
 
                     <C.inputContainer>
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='Nome completo' maxLength={25} value={newPatient.Nome} onChange={(e) => setNewPatient((prev) => ({...prev, Nome: e.target.value}))} />
+                            <input type="text" placeholder='Nome completo' maxLength={25} value={newPatient.Nome} onChange={(e) => setNewPatient((prev) => ({ ...prev, Nome: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="date" value={newPatient.DataDeNascimento} onChange={(e) => setNewPatient((prev) => ({...prev, DataDeNascimento: e.target.value}))} />
+                            <input type="date" value={newPatient.DataDeNascimento} onChange={(e) => setNewPatient((prev) => ({ ...prev, DataDeNascimento: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={newPatient.Genero} onChange={(e) => setNewPatient((prev) => ({...prev, Genero: e.target.value}))}>
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={newPatient.Genero} onChange={(e) => setNewPatient((prev) => ({ ...prev, Genero: e.target.value }))}>
                                 <option>Informe seu gênero</option>
                                 <option value="cisgênero">cisgênero</option>
                                 <option value="transgênero">transgênero</option>
@@ -101,7 +115,7 @@ export const CadastrarPaciente = () => {
                             </select>
 
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={newPatient.Sexo} onChange={(e) => setNewPatient((prev) => ({...prev, Sexo: e.target.value}))}>
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={newPatient.Sexo} onChange={(e) => setNewPatient((prev) => ({ ...prev, Sexo: e.target.value }))}>
                                 <option>Informe seu sexo</option>
                                 <option value="Masculino">Masculino</option>
                                 <option value="Feminino">Feminino</option>
@@ -109,35 +123,35 @@ export const CadastrarPaciente = () => {
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='CPF' minLength={11} maxLength={11}  value={newPatient.CPF} onChange={(e) => setNewPatient((prev) => ({...prev, CPF: e.target.value}))}/>
-                            <input type="tel" placeholder='Telefone' maxLength={11} value={newPatient.Telefone} onChange={(e) => setNewPatient((prev) => ({...prev, Telefone: e.target.value}))} />
+                            <input type="text" placeholder='CPF' minLength={11} maxLength={11} value={newPatient.CPF} onChange={(e) => setNewPatient((prev) => ({ ...prev, CPF: e.target.value }))} />
+                            <input type="tel" placeholder='Telefone' maxLength={11} value={newPatient.Telefone} onChange={(e) => setNewPatient((prev) => ({ ...prev, Telefone: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="email" placeholder='E-mail' maxLength={50} value={newPatient.Email} onChange={(e) => setNewPatient((prev) => ({...prev, Email: e.target.value}))} />
+                            <input type="email" placeholder='E-mail' maxLength={50} value={newPatient.Email} onChange={(e) => setNewPatient((prev) => ({ ...prev, Email: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='Naturalidade' maxLength={25} value={newPatient.Naturalidade} onChange={(e) => setNewPatient((prev) => ({...prev, Naturalidade: e.target.value}))} />
+                            <input type="text" placeholder='Naturalidade' maxLength={25} value={newPatient.Naturalidade} onChange={(e) => setNewPatient((prev) => ({ ...prev, Naturalidade: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='CEP' maxLength={8} value={newPatient.CEP} onChange={(e) => setNewPatient((prev) => ({...prev, CEP: e.target.value}))} />
+                            <input type="text" placeholder='CEP' maxLength={8} value={newPatient.CEP} onChange={(e) => setNewPatient((prev) => ({ ...prev, CEP: e.target.value }))} onBlur={buscarCEP} />
                         </div>
 
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Rua' maxLength={50} value={newPatient.Rua} onChange={(e) => setNewPatient((prev) => ({...prev, Rua: e.target.value}))} />
-                            <input type="text" placeholder='Número' maxLength={10} value={newPatient.Numero} onChange={(e) => setNewPatient((prev) => ({...prev, Numero: e.target.value}))} />
-                        </div>
+                        {newPatient.Rua ? (
+                            <>
+                                <div className="inputContainerItem">
+                                    <input type="text" placeholder='Rua' maxLength={50} value={newPatient.Rua} onChange={(e) => setNewPatient((prev) => ({ ...prev, Rua: e.target.value }))} readOnly />
+                                    <input type="text" placeholder='Número' maxLength={10} value={newPatient.Numero} onChange={(e) => setNewPatient((prev) => ({ ...prev, Numero: e.target.value }))} />
+                                </div>
 
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Bairro' maxLength={50} value={newPatient.Bairro} onChange={(e) => setNewPatient((prev) => ({...prev, Bairro: e.target.value}))} />
-                        </div>
-
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Cidade' maxLength={50} value={newPatient.Cidade} onChange={(e) => setNewPatient((prev) => ({...prev, Cidade: e.target.value}))}/>
-                            <input type="text" placeholder='Estado' maxLength={50} value={newPatient.Estado} onChange={(e) => setNewPatient((prev) => ({...prev, Estado: e.target.value}))} />
-                        </div>
+                                <div className="inputContainerItem">
+                                    <input type="text" placeholder='Cidade' maxLength={50} value={newPatient.Cidade} onChange={(e) => setNewPatient((prev) => ({ ...prev, Cidade: e.target.value }))} readOnly />
+                                    <input type="text" placeholder='Estado' maxLength={50} value={newPatient.Estado} onChange={(e) => setNewPatient((prev) => ({ ...prev, Estado: e.target.value }))} readOnly />
+                                </div>
+                            </>
+                        ) : ''}
 
                         <div className="inputContainerItem">
                             <input type="date" value={date} readOnly />
