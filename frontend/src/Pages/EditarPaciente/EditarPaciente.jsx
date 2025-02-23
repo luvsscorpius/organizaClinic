67875fed64ccbc70b100fdac50e787641eb9dbd4
and <andersonvittor1801@gmail.com>
2025-02-23 15:31:41 -0300

    feat(frontend editarpaciente.jsx): implementing skeletons

--- frontend/src/Pages/EditarPaciente/EditarPaciente.jsx
+++ frontend/src/Pages/EditarPaciente/EditarPaciente.jsx
@@ -5,14 +5,37 @@ import * as C from '../CadastrarMedico/Styles'
 import { FaUserPlus } from "react-icons/fa6";
 import { useNavigate } from 'react-router';
 import { OrganizaClinicContext } from '../../Context/Context';
+import { Box } from '@mui/material'
+import Skeleton from '@mui/material/Skeleton'
+import axios from 'axios'
 
 export const EditarPaciente = () => {
     const navigate = useNavigate()
 
-    const {patientUpdate, setPatientUpdate, sendPatientUpdated} = useContext(OrganizaClinicContext)
+    const { patientUpdate, setPatientUpdate, sendPatientUpdated } = useContext(OrganizaClinicContext)
+
+    // Feat: Implementando busca de endereço através do CEP
+    // Feat: Skeletons
+    const [loading, setLoading] = useState(false)
+
+    const buscarCEP = async () => {
+        if (patientUpdate.CEP.length === 8) {
+            setLoading(true)
+            setTimeout(async () => {
+                try {
+                    const response = await axios.get(`https://viacep.com.br/ws/${patientUpdate.CEP}/json/`)
+                    setPatientUpdate((prev) => ({ ...prev, Rua: response.data.logradouro, Bairro: response.data.bairro, Cidade: response.data.localidade, Estado: response.data.estado }))
+                } catch (error) {
+                    console.error("Erro ao buscar o CEP", error)
+                } finally {
+                    setLoading(false)
+                }
+            }, 1500)
+        }
+    }
 
     return (
-        <H.section style={{flexDirection: 'column'}}>
+        <H.section style={{ flexDirection: 'column' }}>
             <C.ButtonContainer>
                 <span onClick={() => navigate('/Pacientes')}>
                     <MdOutlineArrowBack size={22} />
@@ -28,17 +51,17 @@ export const EditarPaciente = () => {
                     </C.titleContainer>
 
                     <C.inputContainer>
-                    <div className="inputContainerItem">
-                            <input type="text" placeholder='Nome completo' maxLength={25} value={patientUpdate.Nome} onChange={(e) => setPatientUpdate((prev) => ({...prev, Nome: e.target.value}))} />
+                        <div className="inputContainerItem">
+                            <input type="text" placeholder='Nome completo' maxLength={25} value={patientUpdate.Nome} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Nome: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="date" value={patientUpdate.DataDeNascimento} onChange={(e) => setPatientUpdate((prev) => ({...prev, DataDeNascimento: e.target.value}))}/>
+                            <input type="date" value={patientUpdate.DataDeNascimento} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, DataDeNascimento: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={patientUpdate.Genero} onChange={(e) => setPatientUpdate((prev) => ({...prev, Genero: e.target.value}))} >
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={patientUpdate.Genero} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Genero: e.target.value }))} >
                                 <option>Informe seu gênero</option>
                                 <option value="cisgênero">cisgênero</option>
                                 <option value="transgênero">transgênero</option>
@@ -47,7 +70,7 @@ export const EditarPaciente = () => {
                             </select>
 
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={patientUpdate.Sexo} onChange={(e) => setPatientUpdate((prev) => ({...prev, Sexo: e.target.value}))}>
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer" value={patientUpdate.Sexo} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Sexo: e.target.value }))}>
                                 <option>Informe seu sexo</option>
                                 <option value="Masculino">Masculino</option>
                                 <option value="Feminino">Feminino</option>
@@ -55,35 +78,53 @@ export const EditarPaciente = () => {
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='CPF' maxLength={11} value={patientUpdate.CPF} onChange={(e) => setPatientUpdate((prev) => ({...prev, CPF: e.target.value}))} />
+                            <input type="text" placeholder='CPF' maxLength={11} value={patientUpdate.CPF} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, CPF: e.target.value }))} />
                             <input type="tel" placeholder='Telefone' maxLength={11} value={patientUpdate.Telefone} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="email" placeholder='E-mail' maxLength={50} value={patientUpdate.Email} onChange={(e) => setPatientUpdate((prev) => ({...prev, Email: e.target.value}))} />
+                            <input type="email" placeholder='E-mail' maxLength={50} value={patientUpdate.Email} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Email: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='Naturalidade' maxLength={25} value={patientUpdate.Naturalidade} onChange={(e) => setPatientUpdate((prev) => ({...prev, Naturalidade: e.target.value}))} />
+                            <input type="text" placeholder='Naturalidade' maxLength={25} value={patientUpdate.Naturalidade} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Naturalidade: e.target.value }))} />
                         </div>
 
                         <div className="inputContainerItem">
-                            <input type="text" placeholder='CEP' maxLength={8} value={patientUpdate.CEP} onChange={(e) => setPatientUpdate((prev) => ({...prev, CEP: e.target.value}))} />
+                            <input type="text" placeholder='CEP' maxLength={8} value={patientUpdate.CEP} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, CEP: e.target.value }))} onBlur={buscarCEP} />
                         </div>
 
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Rua' maxLength={50} value={patientUpdate.Rua} onChange={(e) => setPatientUpdate((prev) => ({...prev, Rua: e.target.value}))} />
-                            <input type="text" placeholder='Número' maxLength={10} value={patientUpdate.Numero} onChange={(e) => setPatientUpdate((prev) => ({...prev, Numero: e.target.value}))}  />
-                        </div>
+                        {loading ? (
+                            <>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px', width: '100%', padding: '0 10px 0 10px' }}>
+                                    <Skeleton sx={{ width: '100%', height: '60px', padding: '10px' }} />
+                                    <Skeleton sx={{ width: '100%', height: '60px' }} />
+                                </Box>
+                                <Box sx={{ display: 'flex', width: '100%', padding: '0 10px 0 10px' }}>
+                                    <Skeleton sx={{ width: '100%', height: '60px', padding: '10px' }} />
+                                </Box>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px', width: '100%', padding: '0 10px 0 10px' }}>
+                                    <Skeleton sx={{ width: '100%', height: '60px', padding: '10px' }} />
+                                    <Skeleton sx={{ width: '100%', height: '60px' }} />
+                                </Box>
+                            </>
+                        ) : patientUpdate.Rua && (
+                            <>
+                                <div className="inputContainerItem">
+                                    <input type="text" placeholder='Rua' maxLength={50} value={patientUpdate.Rua} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Rua: e.target.value }))} />
+                                    <input type="text" placeholder='Número' maxLength={10} value={patientUpdate.Numero} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Numero: e.target.value }))} />
+                                </div>
 
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Bairro' maxLength={50}  value={patientUpdate.Bairro} onChange={(e) => setPatientUpdate((prev) => ({...prev, Bairro: e.target.value}))} />
-                        </div>
+                                <div className="inputContainerItem">
+                                    <input type="text" placeholder='Bairro' maxLength={50} value={patientUpdate.Bairro} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Bairro: e.target.value }))} />
+                                </div>
 
-                        <div className="inputContainerItem">
-                            <input type="text" placeholder='Cidade' maxLength={50} value={patientUpdate.Cidade} onChange={(e) => setPatientUpdate((prev) => ({...prev, Cidade: e.target.value}))}/>
-                            <input type="text" placeholder='Estado' maxLength={50} value={patientUpdate.Estado} onChange={(e) => setPatientUpdate((prev) => ({...prev, Estado: e.target.value}))}/>
-                        </div>
+                                <div className="inputContainerItem">
+                                    <input type="text" placeholder='Cidade' maxLength={50} value={patientUpdate.Cidade} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Cidade: e.target.value }))} />
+                                    <input type="text" placeholder='Estado' maxLength={50} value={patientUpdate.Estado} onChange={(e) => setPatientUpdate((prev) => ({ ...prev, Estado: e.target.value }))} />
+                                </div>
+                            </>
+                        )}
 
                         <div className="inputContainerItem">
                             <input type="date" value={patientUpdate.DataDeCadastro} readOnly />
